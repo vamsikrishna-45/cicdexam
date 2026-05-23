0a6c3968aabe280adff5f6b4bdb6a8e93dcbaa22
vamsikrishna-45 <182875556+vamsikrishna-45@users.noreply.github.com>
2026-05-23 06:42:26 +0000

    added using intial commit fom main branch

---
+++ Calculator.java
@@ -0,0 +1,64 @@
+class Calculator{
+
+    int add(int a, int b){
+        return a + b;
+    }
+
+    int subtract(int a, int b){
+        return a - b;
+    }
+
+    int multiply(int a, int b){
+        return a * b;
+    }
+
+    int divide(int a, int b){
+        return a / b;
+    }
+
+    int modulus(int a, int b){
+        return a % b;
+    }
+
+    int square(int a){
+        return a * a;
+    }
+
+    int cube(int a){
+        return a * a * a;
+    }
+
+    int maximum(int a, int b){
+        if(a > b)
+            return a;
+        else
+            return b;
+    }
+
+    int minimum(int a, int b){
+        if(a < b)
+            return a;
+        else
+            return b;
+    }
+
+    public static void main(String args[]){
+
+        Calculator c = new Calculator();
+
+        int a = 20;
+        int b = 10;
+
+        System.out.println("Addition = " + c.add(a,b));
+        System.out.println("Subtraction = " + c.subtract(a,b));
+        System.out.println("Multiplication = " + c.multiply(a,b));
+        System.out.println("Division = " + c.divide(a,b));
+        System.out.println("Modulus = " + c.modulus(a,b));
+
+        System.out.println("Square of a = " + c.square(a));
+        System.out.println("Cube of b = " + c.cube(b));
+
+        System.out.println("Maximum Number = " + c.maximum(a,b));
+        System.out.println("Minimum Number = " + c.minimum(a,b));
+    }
+}
--- calculator.java
+++
@@ -1,9 +0,0 @@
-public class Calculator{
-    public static void main(String args[]){
-
-        int a = 10;
-        int b = 5;
-
-        System.out.println("Addition = " + (a+b));
-    }
-}
